b2035f9e36154e7fe5fbe76e14b2169081d29b61
jonathan.deng <dengjonathan@gmail.com>
2017-06-14 09:33:18 -0700

    add location getter

--- OnTheWay/FirstViewController.swift
+++ OnTheWay/FirstViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
-    // MARK: class properties
-    let initialLocation = CLLocation(latitude: 21.282772, longitude: -157.829444)
-    let radius: CLLocationDistance = 1000
+class FirstViewController: UIViewController, CLLocationManagerDelegate {
+  // MARK: class properties
+  let initialLocation = CLLocation(latitude: 21.282772, longitude: -157.829444)
+  let radius: CLLocationDistance = 1000
+  let locationManager: CLLocationManager = CLLocationManager()
 
     // MARK: outlets
     @IBOutlet weak var mapView: MKMapView!
@@ -21,18 +23,34 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
       super.viewDidLoad()
       
+      // center map on initial location and print location
       centerMapOnLocation(on: initialLocation, withRadius: radius)
+      initLocationManager()
+      getLocation()
     }
 
     override func didReceiveMemoryWarning() {
       super.didReceiveMemoryWarning()
       // Dispose of any resources that can be recreated.
     }
-    
+  
+  // MARK: Location based methods
   func centerMapOnLocation(on location: CLLocation, withRadius radius: CLLocationDistance) {
     let diameter = radius * 2
     let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, diameter, diameter)
     mapView.setRegion(coordinateRegion, animated: true)
+  }
+  
+  func initLocationManager() {
+    locationManager.delegate = self
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    locationManager.requestAlwaysAuthorization()
+    locationManager.startUpdatingLocation()
+  }
+  
+  func getLocation() {
+    let locValue: CLLocationCoordinate2D = locationManager.location!.coordinate
+    print("\(locValue.latitude) \(locValue.longitude)")
   }
 }
 
